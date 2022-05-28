e8ff61db186eb205fecf2cec5d319c6596377e0b
kshitij <kmgaikwad97@gmail.com>
2022-05-28 21:43:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nav-links';
+  primary=true
+  secondary=false
+  success=false
+  danger=false
+
+  constructor() {
+
+  }
+  ngOnInit() {
+    $(document).ready(() => {
+      console.log("Jquery is working")
+      //menu clicking
+      $('.linkData').click(function () {
+        $(".linkData").removeClass("active")
+        $(this).addClass('active')
+        console.log($(this))
+        debugger
+        $(".linkData").attr("aria-selected", "false")
+        $(this).attr("aria-selected", "true")
+
+      })
+    }
+    )
+  }
+
+  changeData(val1) {
+    this.primary = false;
+    this.secondary = false;
+    this.success = false;
+    this.danger = false;
+
+    if (val1 == "Primary") {
+      this.primary = true;
+    }
+    if (val1 == "Secondary") {
+      this.secondary = true;
+    }
+    if (val1 == "Success") {
+      this.success = true;
+    }
+    if (val1 == "Danger") {
+      this.danger = true;
+    }
+  }
+
 }
